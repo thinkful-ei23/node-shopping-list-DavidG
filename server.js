e8e725e4d7ab606881d48c62e17af8308d84af9c
David Graves <davidgraves831@gmail.com>
2018-08-01 11:36:51 -0500

    added delete recipe

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+'use strict';
 
 const express = require('express');
 const router = express.Router();
@@ -72,6 +73,11 @@ app.post('/recipes', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
+app.delete('/recipes/:id', (req, res) => {
+  Recipes.delete(req.params.id);
+  console.log(`Deleted recipe item \`${req.params.id}\``);
+  res.status(204).end();
+});
 
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
